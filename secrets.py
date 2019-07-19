d886a2dcad9573d233abd13a86f8854cca983222
Elizabeth Adams <43038812+elizabethn119@users.noreply.github.com>
2019-07-19 10:29:47 -0500

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,16 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid': 'WIFI NAME',
+    'password': 'WIFI PASSWORD',
+    'timezone': 'America/Chicago',
+    'DARKSKY_KEY': 'DARK SKY API KEY',
+    'ACCESS_KEY': 'INITIAL STATE ACCESS KEY',
+    'BUCKET_KEY': 'DarkSkyPyPortal',
+    'BUCKET_NAME': 'DarkSkyPyPortal',
+    'GPS_COORDS': "LONGITUDE,LATITUDE",
+    'CITY_NAME': "CITY NAME, STATE",
+    'aio_username': 'ADAFRUIT IO USERNAME',
+    'aio_key': 'ADAFRUIT IO KEY'
+}
