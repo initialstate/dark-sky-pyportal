d292dd633a9f62882530b4b7d58f6e253e45c220
Elizabeth Adams <43038812+elizabethn119@users.noreply.github.com>
2019-07-19 10:20:26 -0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,86 @@
+"""
+This example queries the DarkSky API to find out the current
+weather for your location... and display it on a screen!
+"""
+import sys
+import time
+import board
+from adafruit_pyportal import PyPortal
+import darksky  # pylint: disable=wrong-import-position
+
+cwd = ("/"+__file__).rsplit('/', 1)[0] # the current working directory (where this file is)
+sys.path.append(cwd)
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+# Set up where we'll be fetching data from
+DATA_SOURCE = "https://api.darksky.net/forecast/"+secrets['DARKSKY_KEY']
+DATA_SOURCE += "/"+secrets['GPS_COORDS']
+DATA_SOURCE += "?units=auto&exclude=hourly,minutely,flags,daily,alerts"
+DATA_LOCATION = []
+
+# Initialize the pyportal object and let us know what data to fetch and where
+# to display it
+pyportal = PyPortal(url=DATA_SOURCE,
+                    json_path=DATA_LOCATION,
+                    status_neopixel=board.NEOPIXEL,
+                    default_bg=0x000000)
+
+gfx = darksky.darksky(pyportal.splash, am_pm=True, celsius=False)
+
+localtile_refresh = None
+weather_refresh = None
+while True:
+    # only query the online time once per hour (and on first run)
+    if (not localtile_refresh) or (time.monotonic() - localtile_refresh) > 3600:
+        try:
+            print("Getting time from internet!")
+            pyportal.get_local_time()
+            localtile_refresh = time.monotonic()
+        except RuntimeError as e:
+            print("Some error occured, retrying! -", e)
+            continue
+
+    # only query the weather every 10 minutes (and on first run)
+    if (not weather_refresh) or (time.monotonic() - weather_refresh) > 600:
+        try:
+            value = pyportal.fetch()
+            print("Response is", value)
+            gfx.display_weather(value)
+            weather_refresh = time.monotonic()
+        except RuntimeError as e:
+            print("Some error occured, retrying! -", e)
+            continue
+
+    gfx.update_time()
+    time.sleep(30)  # wait 30 seconds before updating anything againlocaltile_refresh = None
+weather_refresh = None
+while True:
+    # only query the online time once per hour (and on first run)
+    if (not localtile_refresh) or (time.monotonic() - localtile_refresh) > 3600:
+        try:
+            print("Getting time from internet!")
+            pyportal.get_local_time()
+            localtile_refresh = time.monotonic()
+        except RuntimeError as e:
+            print("Some error occured, retrying! -", e)
+            continue
+
+    # only query the weather every 10 minutes (and on first run)
+    if (not weather_refresh) or (time.monotonic() - weather_refresh) > 600:
+        try:
+            value = pyportal.fetch()
+            print("Response is", value)
+            gfx.display_weather(value)
+            weather_refresh = time.monotonic()
+        except RuntimeError as e:
+            print("Some error occured, retrying! -", e)
+            continue
+
+    gfx.update_time()
+    time.sleep(30)  # wait 30 seconds before updating anything again
